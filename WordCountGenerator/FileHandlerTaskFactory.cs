9e5e924abb377c4e0125c597df1dd74ca79e9ab4
Tim S <tsendgik@gmail.com>
2016-11-07 21:53:04 -0800

    Adds FileHandlerTaskFactory and tests

---
+++ WordCountGenerator/FileHandlerTaskFactory.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace WordCountGenerator
+{
+    public static class FileHandlerTaskFactory
+    {
+        // Must ensure file exists before invoking
+        public static async Task ProcessFile(FileInfo fileToProcess, ConcurrentDictionary<int, int> wordCounts)
+        {
+            if (fileToProcess == null)
+            {
+                throw new ArgumentNullException("fileToProcess");
+            }
+            
+            if (wordCounts == null)
+            {
+                throw new ArgumentNullException("wordCounts");
+            }
+
+            if (!fileToProcess.Exists)
+            {
+                Console.WriteLine("File {0} does not exist", fileToProcess.FullName);
+                return;
+            }
+
+            if (fileToProcess.Extension.Equals(TextFileHandler.TextFileExtension))
+            {
+                int wordCount = await TextFileHandler.GetWordCount(fileToProcess);
+
+                if (wordCounts.ContainsKey(wordCount))
+                {
+                    wordCounts[wordCount]++;
+                }
+                else
+                {
+                    wordCounts.TryAdd(wordCount, 1);
+                }
+            }
+            else if (fileToProcess.Extension.Equals(ArchiveFileHandler.ArchiveExtension))
+            {
+                List<int> archiveWordCounts = (List<int>) await ArchiveFileHandler.GetWordCount(fileToProcess);
+
+                foreach (int wordCount in archiveWordCounts)
+                {
+                    if (wordCounts.ContainsKey(wordCount))
+                    {
+                        wordCounts[wordCount]++;
+                    }
+                    else
+                    {
+                        wordCounts.TryAdd(wordCount, 1);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine(
+                    String.Format(
+                        "Ignoring file type {0} encountered on {1}", 
+                        fileToProcess.Extension, 
+                        fileToProcess.FullName)
+                        );
+            }
+        }
+    }
+}
---
+++ WordCountTests/FileHandlerTaskFactoryTests.cs
@@ -0,0 +1,85 @@
+﻿using System.Threading.Tasks;
+using System.IO;
+using System.Collections.Concurrent;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordCountGenerator;
+
+namespace FileHandlerTaskFactoryTests
+{
+    [TestClass]
+    public class FileHandlerTaskFactoryTests
+    {
+        [TestMethod]
+        [DeploymentItem(@".\SampleInputFiles\George Berkeley - Principles of Human Knowledge.txt")]
+        public async Task ProcessFileTest_SingleTextFile()
+        {
+            string inputFile = @".\George Berkeley - Principles of Human Knowledge.txt";
+
+            ConcurrentDictionary<int, int> wordCounts = new ConcurrentDictionary<int, int>();
+            FileInfo fi = new FileInfo(inputFile);
+            await FileHandlerTaskFactory.ProcessFile(fi, wordCounts);
+
+            Assert.AreEqual(1, wordCounts[36600]);
+        }
+
+        [TestMethod]
+        [DeploymentItem(@".\SampleInputFiles\George Berkeley - Three Dialogues.txt")]
+        public async Task ProcessFileTest_SingleTextFile_Increment()
+        {
+            string inputFile = @".\George Berkeley - Three Dialogues.txt";
+
+            ConcurrentDictionary<int, int> wordCounts = new ConcurrentDictionary<int, int>();
+            wordCounts.TryAdd(36532, 2);
+
+            FileInfo fi = new FileInfo(inputFile);
+            await FileHandlerTaskFactory.ProcessFile(fi, wordCounts);
+
+            Assert.AreEqual(3, wordCounts[36532]);
+        }
+
+        [TestMethod]
+        [DeploymentItem(@".\SampleInputFiles\NestedFiles.zip")]
+        public async Task ProcessFileTest_SingleArchiveFile()
+        {
+            string inputFile = @".\NestedFiles.zip";
+
+            FileInfo fi = new FileInfo(inputFile);
+            ConcurrentDictionary<int, int> wordCounts = new ConcurrentDictionary<int, int>();
+            await FileHandlerTaskFactory.ProcessFile(fi, wordCounts);
+
+            Assert.AreEqual(6, wordCounts.Count);
+            Assert.AreEqual(1, wordCounts[36600]);
+            Assert.AreEqual(1, wordCounts[36532]);
+
+            Assert.AreEqual(1, wordCounts[193392]);
+            Assert.AreEqual(1, wordCounts[183062]);
+
+            Assert.AreEqual(1, wordCounts[140548]);
+            Assert.AreEqual(1, wordCounts[117320]);
+        }
+
+        [TestMethod]
+        [DeploymentItem(@".\SampleInputFiles\NestedFiles.zip")]
+        public async Task ProcessFileTest_SingleArchiveFile_Increment()
+        {
+            string inputFile = @".\NestedFiles.zip";
+            
+            ConcurrentDictionary<int, int> wordCounts = new ConcurrentDictionary<int, int>();
+            wordCounts.TryAdd(36600, 5);
+            wordCounts.TryAdd(193392, 3);
+
+            FileInfo fi = new FileInfo(inputFile);
+            await FileHandlerTaskFactory.ProcessFile(fi, wordCounts);
+
+            Assert.AreEqual(6, wordCounts.Count);
+            Assert.AreEqual(6, wordCounts[36600]);
+            Assert.AreEqual(1, wordCounts[36532]);
+
+            Assert.AreEqual(4, wordCounts[193392]);
+            Assert.AreEqual(1, wordCounts[183062]);
+
+            Assert.AreEqual(1, wordCounts[140548]);
+            Assert.AreEqual(1, wordCounts[117320]);
+        }
+    }
+}
