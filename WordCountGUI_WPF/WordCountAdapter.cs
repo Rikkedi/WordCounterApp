70c46f829a15cd8da3dc50a2219e7768e773ec16
Tim S <tsendgik@gmail.com>
2016-11-11 15:58:52 -0800

    Adds bare-bones model class for wrapping the data processor.

---
+++ WordCountGUI_WPF/WordCountAdapter.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using WordCountGenerator;
+
+namespace WordCountGUI_WPF
+{
+    internal class WordCountAdapter
+    {
+        private FolderHandler wordCountGenerator;
+
+        public ConcurrentDictionary<long, int> WordCounts
+        {
+            get
+            {
+                if (this.wordCountGenerator == null)
+                {
+                    return null;
+                }
+
+                return this.wordCountGenerator.FileCountsByWordCount;
+            }
+        }
+
+        public WordCountAdapter(string folderPath)
+        {
+            this.wordCountGenerator = new FolderHandler(folderPath);
+        }
+
+        public async Task DiscoverFiles()
+        {
+            if (this.wordCountGenerator == null)
+            {
+                return;
+            }
+
+            await this.wordCountGenerator.DiscoverFilesToProcess();
+        }
+
+        public async Task ProcessFiles()
+        {
+            if (this.wordCountGenerator == null)
+            {
+                return;
+            }
+
+            await this.wordCountGenerator.ProcessFiles();
+        }
+    }
+}
