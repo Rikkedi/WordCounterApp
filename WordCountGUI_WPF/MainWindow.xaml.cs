ce6e338850b2c69b6d6f8fbf1b89b17a934c3006
Tim S <tsendgik@gmail.com>
2016-11-20 18:11:54 -0800

    Adds basic call to backend code for processing and starts implementing the elements for the UI.

--- WordCountGUI_WPF/MainWindow.xaml.cs
+++ WordCountGUI_WPF/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -12,17 +13,42 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace WordCountGUI_WPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : NavigationWindow
+    public partial class MainWindow : Window
     {
+        private FolderBrowserDialog folderBrowser;
+        private WordCountAdapter wordCounts;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            this.folderBrowser = new FolderBrowserDialog();
+            this.folderBrowser.Description = "Select the directory that you want to scan.";
+            this.folderBrowser.ShowNewFolderButton = false;
+        }
+
+        private void OpenFolder_Button_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult result = this.folderBrowser.ShowDialog();
+
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                string folderPath = this.folderBrowser.SelectedPath;
+
+                this.wordCounts = new WordCountAdapter(folderPath);
+
+                // TODO - Make this non-blocking
+                this.wordCounts.DiscoverFiles().Wait();
+                this.wordCounts.ProcessFiles().Wait();
+                Console.WriteLine("Done!");
+            }
         }
     }
 }
